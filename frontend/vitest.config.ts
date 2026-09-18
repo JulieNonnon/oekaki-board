af1816b040faf12f56e4c4be9291a8794f8b47f7
JulieNonnon <julie.nonnon@gmail.com>
2026-09-18 23:53:04 +0200

    feat(frontend-test): install and config vitest.setup.ts for React component testing

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    setupFiles: "./vitest.setup.ts", // ce fichier sera lancé avant les tests
   },
 });
---
+++ frontend/vitest.setup.ts
@@ -0,0 +1,3 @@
+// à lier dans vitest.config.ts pour que les tests unitaires puissent utiliser les matchers de jest-dom
+
+import "@testing-library/jest-dom/vitest";
