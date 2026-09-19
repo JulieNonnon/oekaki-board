73463bbf10d0b8cb044b3e765e5e4ec364fc7a66
JulieNonnon <julie.nonnon@gmail.com>
2026-09-19 22:25:55 +0200

    feat(frontend-test): test user interaction on save button

--- frontend/src/components/drawing/SaveBar.test.tsx
+++ frontend/src/components/drawing/SaveBar.test.tsx
@@ -1,6 +1,7 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { SaveBar } from "./SaveBar";
+import userEvent from "@testing-library/user-event";
 
 describe("SaveBar", () => {
   it("should disable the Save button when drawing cannot be saved", () => {
@@ -35,5 +36,26 @@ describe("SaveBar", () => {
     });
 
     expect(saveButton).toBeEnabled();
+  });
+  it("should call onSave when the user clicks Save", async () => {
+    const onSave = vi.fn(); // mock
+    const user = userEvent.setup(); // création d'un user virtuel pour simuler les interactions utilisateur
+
+    render(
+        <SaveBar
+        title="Mon dessin"
+        setTitle={() => {}}
+        onSave={onSave}
+        canSave={true}
+        />
+    );
+
+    const saveButton = screen.getByRole("button", {
+        name: /save/i,
+    });
+
+    await user.click(saveButton); // Le await est important : userEvent simule les interactions utilisateur de manière asynchrone afin de se rapprocher du comportement réel du navigateur.
+
+    expect(onSave).toHaveBeenCalled();
     });
 });
